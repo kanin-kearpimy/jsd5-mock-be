9ace2c82702e4750ec9fb52c9a0d6b0bad2557aa
kanin-kearpimy <kanin.kearpimy@gmail.com>
2023-09-12 13:09:28 +0700

    backend for react assessment

--- main-old.py
+++ main-old.py
@@ -19,32 +19,50 @@ app.add_middleware(
 data = [
     {
         "id": "cc9754c4-f619-4921-bf7e-8a4fc9714e7f",
-        "name": "mock",
-        "lastname": "mocklastname",
-        "position": "Manager",
+        "name": "Mock Name",
+        "age": 99,
+        "weight": 63.5,
+        "status": "health",
     },
     {
         "id": "66a3f853-5c09-4412-a380-3cc1ebf08c9c",
-        "name": "employee 1",
-        "lastname": "em",
-        "position": "Engineer",
+        "name": "John Doe",
+        "age": 35,
+        "weight": 78.2,
+        "status": "active",
     },
     {
         "id": "64715e05-b45b-4aba-b6d2-d7a4f61ccf0a",
-        "name": "employee 2",
-        "lastname": "lord",
-        "position": "Designer",
+        "name": "Alice Smith",
+        "age": 28,
+        "weight": 54.9,
+        "status": "health",
+    },
+    {
+        "id": "61ec7ef2-f2e5-460c-bfd0-c060a99d0f50",
+        "name": "Jane Johnson",
+        "age": 42,
+        "weight": 67.0,
+        "status": "recovered",
+    },
+    {
+        "id": "6b72fbe1-5c72-42bb-a25f-8d00cef7e88a",
+        "name": "Bob Brown",
+        "age": 55,
+        "weight": 80.7,
+        "status": "health",
     },
 ]
 
-KEYS = ["id", "name", "lastname", "position"]
+KEYS = ["id", "name", "age", "weight", "status"]
 
 
 class Member(BaseModel):
     id: uuid.UUID = uuid.uuid4()
     name: str
-    lastname: str
-    position: str
+    age: int
+    weight: float
+    status: str
 
 
 @app.get("/")
@@ -55,6 +73,17 @@ def root():
 @app.get("/members")
 def get():
     return data
+
+
+@app.get("/members/{member_id}")
+def get_by_id(member_id: str):
+    global data
+    new_data = filter(lambda member: member["id"] == member_id, data)
+    one_data = list(new_data)
+    if len(one_data) == 0:
+        return {"message": "Not found"}
+
+    return one_data[0]
 
 
 @app.post("/members")
